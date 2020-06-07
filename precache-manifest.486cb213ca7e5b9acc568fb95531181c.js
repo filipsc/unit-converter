e3d61faeaa777ad2ebdbc618faa2655be5fc2ea8
Filip Schulze <filipsc@kth.se>
2020-06-07 19:02:11 +0200

    Updates

---
+++ precache-manifest.486cb213ca7e5b9acc568fb95531181c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c766e7bab8fa8cc98d964ee7464f5e",
+    "url": "/unit-converter/index.html"
+  },
+  {
+    "revision": "ce92e10efbad6633a09a",
+    "url": "/unit-converter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "acb8276d4a036f1a69b8",
+    "url": "/unit-converter/static/js/2.69c3ee62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/unit-converter/static/js/2.69c3ee62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce92e10efbad6633a09a",
+    "url": "/unit-converter/static/js/main.743c7d98.chunk.js"
+  },
+  {
+    "revision": "cd1c83d563988e191858",
+    "url": "/unit-converter/static/js/runtime-main.5d27433f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/unit-converter/static/media/logo.5d5d9eef.svg"
+  }
+]);
